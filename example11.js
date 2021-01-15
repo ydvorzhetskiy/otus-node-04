6ba85a69d2699f00e7bc564ce77c0dfb5e68b2e8
ydvorzhetskiy <dvorzhetskij@mail.ru>
2021-01-15 19:32:45 +0300

    Factorial

---
+++ example11.js
@@ -0,0 +1,14 @@
+const factorial = function* () {
+    let result = 1;
+    let i = 1;
+    while(true) {
+      result *= i++;
+      if (result > 10000) return result;
+      yield result;
+    }
+    return;
+}
+
+for (const value of factorial()) {
+    console.log(value);
+}
